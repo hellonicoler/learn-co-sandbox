e63ca8374d55f1720961a98ac5996acb6725680e
Nicole Rothenay <nthorn1211@gmail.com>
2020-08-27 20:31:03 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,10 @@
+
+# Defines loop
+loop do
+  puts "Never pass a school bus if it has flashing red lights"
+end
+
+# Calls loop method
+looping
+
+# Loop is infinite: escape with control + c
---
+++ my_new_directory/README.md
@@ -0,0 +1,3 @@
+This is my readme file
+
+Still having a bit of trouble!
